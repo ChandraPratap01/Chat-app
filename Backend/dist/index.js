4a799699162c4198831513bf3622d5221516bea5
Chandra Chauhan <chandrapratap0001@gmail.com>
2025-04-19 14:16:24 +0530

    update_tsConfig.js

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -4,6 +4,7 @@ const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 const users = [];
 wss.on("connection", (socket) => {
+    console.log("user connected");
     socket.on("error", console.error);
     socket.on("message", (message) => {
         var _a;
@@ -16,11 +17,19 @@ wss.on("connection", (socket) => {
             });
         }
         if (parsemessage.type == "chat") {
-            const CurrRoom = (_a = users.find((x) => x.socket == socket)) === null || _a === void 0 ? void 0 : _a.roomId;
-            const SendAll = users.filter((x) => x.roomId == CurrRoom);
+            const CurrRoom = (_a = users.find((x) => x.socket === socket)) === null || _a === void 0 ? void 0 : _a.roomId;
+            const SendAll = users.filter((x) => x.roomId === CurrRoom);
             SendAll.forEach((user) => {
                 user.socket.send(parsemessage.payload.message);
             });
         }
     });
+    socket.on("close", () => {
+        const curruser = users.findIndex((x) => x.socket === socket);
+        if (curruser == -1)
+            return;
+        users.splice(curruser, 1);
+        console.log("user left");
+    });
+    socket.send("Welcome to the chat room!");
 });
